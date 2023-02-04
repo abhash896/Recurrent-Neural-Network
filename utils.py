5869c002a126d6169316eb247aa0c6ce57b60978
abhas <abhash896@gmail.com>
2023-02-05 00:14:52 +0530

    modified utils.py

--- utils.py
+++ utils.py
@@ -112,9 +112,6 @@ def rnn_forward(x, a0, parameters):
 
 # LSTM (Long short term memory) functions:
 
-# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-# GRADED FUNCTION: lstm_cell_forward
-
 def lstm_cell_forward(xt, a_prev, c_prev, parameters):
     """
     Implement a single forward step of the LSTM-cell as described in Figure (4)
@@ -181,3 +178,70 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
     cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
 
     return a_next, c_next, yt_pred, cache
+
+
+def lstm_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network using an LSTM-cell described in Figure (4).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc -- Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo -- Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+                        
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    c -- The value of the cell state, numpy array of shape (n_a, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of all the caches, x)
+    """
+
+    # Initialize "caches", which will track the list of all the caches
+    caches = []
+    
+    ### START CODE HERE ###
+    Wy = parameters['Wy'] # saving parameters['Wy'] in a local variable in case students use Wy instead of parameters['Wy']
+    # Retrieve dimensions from shapes of x and parameters['Wy'] (≈2 lines)
+    n_x, m, T_x = x.shape
+    n_y, n_a = Wy.shape
+    
+    # initialize "a", "c" and "y" with zeros (≈3 lines)
+    a = np.zeros((n_a, m, T_x))
+    c = np.zeros((n_a, m, T_x))
+    y = np.zeros((n_y, m, T_x))
+    
+    # Initialize a_next and c_next (≈2 lines)
+    a_next = a0
+    c_next = np.zeros((n_a, m))
+    
+    # loop over all time-steps
+    for t in range(T_x):
+        # Get the 2D slice 'xt' from the 3D input 'x' at time step 't'
+        xt = x[:,:,t]
+        # Update next hidden state, next memory state, compute the prediction, get the cache (≈1 line)
+        a_next, c_next, yt, cache = lstm_cell_forward(xt, a_next, c_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:,:,t] = a_next
+        # Save the value of the next cell state (≈1 line)
+        c[:,:,t]  = c_next
+        # Save the value of the prediction in y (≈1 line)
+        y[:,:,t] = yt
+        # Append the cache into caches (≈1 line)
+        caches.append(cache)
+        
+    ### END CODE HERE ###
+    
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+
+    return a, y, c, caches
